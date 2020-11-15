066f540e67debe9e13203fe0bcecc7c7c2a1cdc0
mikeyMike6 <71586476+mikeyMike6@users.noreply.github.com>
2020-11-15 02:43:31 +0100

    com

---
+++ PIII-Kolokwium1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PIII_Kolokwium1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
